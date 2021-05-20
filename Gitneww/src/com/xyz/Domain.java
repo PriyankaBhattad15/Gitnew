3ac5e686f6dc9a043b888a533b489ccb4ad35f46
PriyankaBhattad15 <priyankabhattad15@gmail.com>
2021-05-20 14:34:59 +0530

    fils added to local

---
+++ Gitneww/src/com/xyz/Domain.java
@@ -0,0 +1,11 @@
+package com.xyz;
+
+public class Domain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("shine in the sky");
+		System.out.println("nice decor");
+	}
+
+}
